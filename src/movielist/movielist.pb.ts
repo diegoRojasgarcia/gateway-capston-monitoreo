893ebee3a57e09aae687c6767833040a268915d5
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-11-22 18:46:52 -0300

    update

--- src/movielist/movielist.pb.ts
+++ src/movielist/movielist.pb.ts
@@ -5,27 +5,22 @@ export const MOVIELIST_SERVICE_NAME = 'MovielistService';
 export const MOVIELIST_PACKAGE_NAME = 'movielist';
 
 export interface MovielistServiceClient {
-    createMovielist(CreateMovielistRequest): Observable<CreateMovieslistResponse>;
+  createMovielist(CreateMovielistRequest): Observable<CreateMovieslistResponse>;
 }
-
 
 export interface CreateMovielistRequest {
-    idUser: number;
-    nombre: string;
+  idUser: number;
+  nombre: string;
 }
 
-
 export interface Movielist {
-    id: number;
-    nombre: string;
-    idUser: number;
-  }
-
+  id: number;
+  nombre: string;
+  idUser: number;
+}
 
 export interface CreateMovieslistResponse {
-    status: number;
-    error: string[];
-    data: Movielist | undefined;
-  }
-  
-
+  status: number;
+  error: string[];
+  data: Movielist | undefined;
+}
