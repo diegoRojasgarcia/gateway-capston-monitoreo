c878c5d130ab3bc1b8ece1214647ba6e462119a1
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-11-29 01:07:00 -0300

    update

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -17,26 +17,18 @@ export class AuthGuard implements CanActivate {
 
   public async canActivate(ctx: ExecutionContext): Promise<boolean> | never {
     const req: Request = ctx.switchToHttp().getRequest();
-
-
     const authorization: string = req.headers['authorization'];
-
     if (!authorization) {
       throw new UnauthorizedException();
     }
-
     const bearer: string[] = authorization.split(' ');
-
     if (!bearer || bearer.length < 2) {
       throw new UnauthorizedException();
     }
-
     const token: string = bearer[1];
-
-
-    const { status, userId }: ValidateResponse = await this.service.validate({token});
-
-
+    const { status, userId }: ValidateResponse = await this.service.validate({
+      token,
+    });
     if (status !== HttpStatus.OK) {
       throw new UnauthorizedException();
     }
