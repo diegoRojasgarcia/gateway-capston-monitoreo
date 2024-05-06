fb45b822cc95a6d490542d3534682c6c8fc95392
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2024-05-06 18:47:07 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Use this to remove cors errors from client side request
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    if (req.method == 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET',
+      );
+      return res.status(200).json({});
+    }
+    next();
+  });
   app.enableCors();
   await app.listen(4000);
 }
