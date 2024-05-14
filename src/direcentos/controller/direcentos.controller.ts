a2748e8d9175e364383c804b5491c9d5a8deb2f4
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2024-05-14 00:39:18 -0400

    update

--- src/direcentos/controller/direcentos.controller.ts
+++ src/direcentos/controller/direcentos.controller.ts
@@ -17,7 +17,7 @@ export class DirecentosController {
     return this.direcentosService.getDirectorios();
   }
 
-  @Post('/date')
+  @Post('/dates')
   private async getDirectoriosfecha(
     @Body() body: labInput,
   ): Promise<datesResponse> {
